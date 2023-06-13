3785ff63361b4a2deaaa9f2a7bb600bdb2c90821
Dudarenko Dmitry <dvd2005.78@gmail.com>
2023-06-13 23:15:51 +0300

    Final version. I hope :)

--- src/pl/senla/lecture3/task3/Main.java
+++ src/pl/senla/lecture3/task3/Main.java
@@ -2,32 +2,14 @@ package pl.senla.lecture3.task3;
 
 import pl.senla.lecture3.task3.assembly.AssemblyLineImpl;
 import pl.senla.lecture3.task3.assembly.IAssemblyLine;
-import pl.senla.lecture3.task3.building.ILineStep;
-import pl.senla.lecture3.task3.building.LineStepBody;
-import pl.senla.lecture3.task3.building.LineStepChassis;
-import pl.senla.lecture3.task3.building.LineStepEngine;
-import pl.senla.lecture3.task3.part.IProductPart;
 import pl.senla.lecture3.task3.product.IProduct;
 import pl.senla.lecture3.task3.product.ProductImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Production of auto components is started.");
-        ILineStep body = new LineStepBody();
-        ILineStep chassis = new LineStepChassis();
-        ILineStep engine = new LineStepEngine();
-
-        IProductPart firstProductPart = body.buildProductPart();
-        IProductPart secondProductPart = chassis.buildProductPart();
-        IProductPart thirdProductPart = engine.buildProductPart();
-        System.out.println("Production of auto components was completed.\n");
-
+        System.out.println("Production is started.");
         IProduct product = new ProductImpl();
-        product.installFirstPart(firstProductPart);
-        product.installSecondPart(secondProductPart);
-        product.installThirdPart(thirdProductPart);
-
         IAssemblyLine assemblyLine = new AssemblyLineImpl();
         System.out.println("New product: " + assemblyLine.assembleProduct(product));
 
