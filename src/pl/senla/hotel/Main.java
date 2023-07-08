f58c47bc2fd7d54c5a5fc1587f05007eecdbe0d2
Dudarenko Dmitry <dvd2005.78@gmail.com>
2023-07-08 12:24:50 +0100

    TASK_5: Change Class Main.

--- src/pl/senla/hotel/Main.java
+++ src/pl/senla/hotel/Main.java
@@ -7,9 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-//        Console consoleCollection = new ConsoleCollection(); // version 1 (without UI)
-//        Console consoleCollection = new ConsoleUI(); // version 2 (with UI)
-//        consoleCollection.startMainMenu(); // version 2 (with UI)
+        /**
+         * previous versions:
+        Console consoleCollection = new ConsoleCollection(); // version 1 (without UI)
+        Console consoleCollection = new ConsoleUI(); // version 2 (with UI)
+        consoleCollection.startMainMenu(); // version 2 (with UI)
+         */
 
         StartMenu startMenuMain = new StartMenuMain(); // version 3 (UI with separate menus for every operation)
         startMenuMain.runMenu();
