0e3cce6ec82b6e38374703764dd1449d97b81df8
Dudarenko Dmitry <dvd2005.78@gmail.com>
2023-07-26 13:03:56 +0300

    TASK_5 REFACTORING. Add to HotelService.java new attribute: idOrder. Change appropriate subclasses.

--- src/pl/senla/hotel/entity/services/HotelService.java
+++ src/pl/senla/hotel/entity/services/HotelService.java
@@ -1,22 +1,28 @@
 package pl.senla.hotel.entity.services;
 
+import static pl.senla.hotel.constant.OrderConstant.ERROR_READ_ORDER;
 import static pl.senla.hotel.constant.RoomReservationConstant.ERROR_CREATE_ROOM_RESERVATION_NO_CLIENT;
 
 public abstract class HotelService {
 
-    protected int idService = -1; // new
+    protected int idService = -1;
+    protected int idOrder;
     protected String typeOfService;
     protected int idGuest;
 
     protected HotelService() {
     }
 
-    protected HotelService(String typeOfService, int idGuest) {
-        if (idGuest == -1) {
+    protected HotelService(int idService, int idOrder, String typeOfService, int idGuest) {
+        if(idOrder < 0) {
+            System.out.println(ERROR_READ_ORDER);
+            return;
+        }
+        if (idGuest < 0) {
             System.out.println(ERROR_CREATE_ROOM_RESERVATION_NO_CLIENT);
             return;
         }
-        this.idService = idService; // new
+        this.idService = idService;
         this.idGuest = idGuest;
         this.typeOfService = typeOfService;
     }
@@ -27,6 +33,14 @@ public abstract class HotelService {
 
     public void setIdService(int idService) {
         this.idService = idService;
+    }
+
+    public int getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(int idOrder) {
+        this.idOrder = idOrder;
     }
 
     public String getTypeOfService() {
