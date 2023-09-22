9de5110c9e0b8d1f9e0943e2a026734c9ae6fc00
Dudarenko Dmitry <102149674+dudarenko-dmitry@users.noreply.github.com>
2023-09-22 12:59:57 +0300

    Task_7: 1. edited properties names. 2. deleted unnecessary lines in: public Properties loadConfiguration()

--- src/pl/senla/hotel/configuration/AppPropertiesLoader.java
+++ src/pl/senla/hotel/configuration/AppPropertiesLoader.java
@@ -15,24 +15,6 @@ public class AppPropertiesLoader implements PropertiesLoader {
     public Properties loadConfiguration() {
         try (FileInputStream fis = new FileInputStream(propertiesFile)) {
             appProperties.load(fis);
-            appProperties.put(KEY_ABLE_TO_CHANGE_ROOM_STATUS,
-                    appProperties.getProperty(KEY_ABLE_TO_CHANGE_ROOM_STATUS));
-            appProperties.put(KEY_NUMBER_OF_GUEST_RECORDS_FOR_ROOM,
-                    appProperties.getProperty(KEY_NUMBER_OF_GUEST_RECORDS_FOR_ROOM));
-            appProperties.put(KEY_FILE_PATH,
-                    appProperties.getProperty(KEY_FILE_PATH));
-            appProperties.put(KEY_FILE_PATH_SERIALIZABLE,
-                    appProperties.getProperty(KEY_FILE_PATH_SERIALIZABLE));
-            appProperties.put(KEY_FILE_SERIALIZABLE_NAME,
-                    appProperties.getProperty(KEY_FILE_SERIALIZABLE_NAME));
-            appProperties.put(KEY_FILE_HOTEL_FACILITIES_NAME,
-                    appProperties.getProperty(KEY_FILE_HOTEL_FACILITIES_NAME));
-            appProperties.put(KEY_FILE_GUESTS_NAME,
-                    appProperties.getProperty(KEY_FILE_GUESTS_NAME));
-            appProperties.put(KEY_FILE_HOTEL_SERVICES_NAME,
-                    appProperties.getProperty(KEY_FILE_HOTEL_SERVICES_NAME));
-            appProperties.put(KEY_FILE_ORDERS_NAME,
-                    appProperties.getProperty(KEY_FILE_ORDERS_NAME));
         } catch (FileNotFoundException e) {
             System.out.println(ERROR_GET_PROPERTIES_FILE);
         } catch (IOException e) {
