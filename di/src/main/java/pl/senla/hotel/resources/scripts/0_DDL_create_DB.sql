e69beab2c4d42dddab60b71dace464d03ed58ee8
Dudarenko Dmitry <102149674+dudarenko-dmitry@users.noreply.github.com>
2023-11-26 23:19:02 +0000

    Task_12-2: Modules were refactored.

--- di/src/main/java/pl/senla/hotel/resources/scripts/0_DDL_create_DB.sql
+++
@@ -1,7 +0,0 @@
--- DB hotel definition
-
-CREATE database IF NOT EXISTS hotel;
-
----- do we need this?
---CREATE USER 'root' IDENTIFIED BY 'Kbktnf2003!';
---GRANT ALL on hotel.* TO 'root';
--- di/src/main/java/pl/senla/hotel/resources/scripts/1_DDL_create_table_guests.sql
+++
@@ -1,7 +0,0 @@
--- hotel.guests definition
-
-CREATE TABLE IF NOT EXISTS hotel.guests (
-  `guestID` int AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(50) NOT NULL,
-  `phone` int NOT NULL
-);
