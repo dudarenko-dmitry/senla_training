dd92b6bae5be8791ab9563c8ead5f59bb2501d19
Dudarenko Dmitry <102149674+dudarenko-dmitry@users.noreply.github.com>
2023-12-11 14:04:48 +0000

    Task_14: Task is completed

--- ui/src/main/java/pl/senla/hotel/Main.java
+++ ui/src/main/java/pl/senla/hotel/Main.java
@@ -4,9 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import pl.senla.hotel.ui.main.StartMenuMainDB;
 
 import java.lang.reflect.InvocationTargetException;
@@ -15,9 +12,6 @@ import static pl.senla.hotel.constant.ApplicationContextConstant.START_APPLICATI
 
 @Slf4j
 @SpringBootApplication
-@ComponentScan("pl.senla.hotel")
-@EnableJpaRepositories("pl.senla.hotel.dao")
-@PropertySource("classpath:application.properties")
 public class Main {
 
     public static void main(String[] args)
--- ui/src/main/java/pl/senla/hotel/SpringConfig.java
+++ ui/src/main/java/pl/senla/hotel/SpringConfig.java
@@ -3,10 +3,12 @@ package pl.senla.hotel;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan("pl.senla.hotel")
 @PropertySource("application.properties")
+@EnableJpaRepositories("pl.senla.hotel.dao")
 public class SpringConfig {
 
 }
